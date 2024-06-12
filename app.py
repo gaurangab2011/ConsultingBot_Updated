94b3f5909327e3f8f6eca4487bd45d39e7f9983f
gaurangab2011 <119389378+gaurangab2011@users.noreply.github.com>
2024-06-12 11:34:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ class TrackableUserProxyAgent(UserProxyAgent):
 
 with st.sidebar:
     st.header("OpenAI Configuration")
-    selected_model = st.selectbox("Model", ['gpt-3.5-turbo', 'gpt-4'], index=1)
+    selected_model = st.selectbox("Model", ['gpt-3.5-turbo', 'gpt-4', 'gpt-4o'], index=1)
     selected_key = st.text_input("API Key", type="password")
 
 with st.sidebar:
